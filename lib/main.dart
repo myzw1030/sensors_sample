86af17c64acd5130af38962e81b33196b06e2878
miyazawa <13tmtmtommy@gmail.com>
2023-12-02 17:21:11 +0900

    調整

--- lib/main.dart
+++ lib/main.dart
@@ -1,61 +1,79 @@
 import 'package:flutter/material.dart';
 import 'package:sensors_plus/sensors_plus.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(const MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demoああ',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    return const MaterialApp(
+      home: HomePage(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  double dx = 100;
-  double dy = 100;
+class _HomePageState extends State<HomePage> {
+  double dx = 0;
+  double dy = 0;
+  final double ballDiameter = 50;
+
+  @override
+  void initState() {
+    super.initState();
+    accelerometerEventStream().listen((AccelerometerEvent event) {
+      setState(() {
+        // ボールの位置を更新
+        dx -= event.x * 50;
+        dy += event.y * 50;
+        // 画面の範囲を超えないように座標は 0 と (画面の幅 - ボールの直径) の間に制限
+        dx = dx.clamp(0, MediaQuery.sizeOf(context).width - ballDiameter);
+        dy = dy.clamp(0, MediaQuery.sizeOf(context).height - ballDiameter);
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
+      body: Stack(
+        children: <Widget>[
+          AnimatedPositioned(
+            duration: const Duration(milliseconds: 60),
+            left: dx,
+            top: dy,
+            child: BallWidget(ballDiameter: ballDiameter),
+          ),
+        ],
       ),
-      body: StreamBuilder<GyroscopeEvent>(
-        stream: SensorsPlatform.instance.gyroscopeEventStream(),
-        builder: (_, snapshot) {
-          if (snapshot.hasData) {
-            dx = dx + (snapshot.data!.y * 10);
-            dy = dy + (snapshot.data!.x * 10);
-          }
-          return Transform.translate(
-            offset: Offset(dx, dy),
-            child: const CircleAvatar(
-              backgroundColor: Colors.red,
-              radius: 20,
-            ),
-          );
-        },
+    );
+  }
+}
+
+class BallWidget extends StatelessWidget {
+  const BallWidget({
+    super.key,
+    required this.ballDiameter,
+  });
+
+  final double ballDiameter;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: ballDiameter,
+      height: ballDiameter,
+      decoration: const BoxDecoration(
+        color: Colors.red,
+        shape: BoxShape.circle,
       ),
     );
   }
